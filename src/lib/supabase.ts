bd278c52968f935a1efb1bf5c939638bb1388251
Amir-Benhadid <amir.benhadid@outlook.com>
2025-03-31 03:39:58 -0400

    update env files

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,9 @@
 import { Database } from '@/types/supabase';
 import { createClient } from '@supabase/supabase-js';
 
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-	throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL');
-}
-if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-	throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY');
-}
+const url = 'https://crmfdcioxwviuendnlik.supabase.co';
+const key =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNybWZkY2lveHd2aXVlbmRubGlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUyMzk5MDEsImV4cCI6MjA1MDgxNTkwMX0.SXlBRQqbPMbIDkXIKn55Imwu0GAZJEgJF2tNertFAJc';
 
 // Create a storage object that safely works in both browser and server contexts
 const storage =
@@ -19,18 +16,14 @@ const storage =
 		  };
 
 // Create the Supabase client with proper persistence settings
-export const supabase = createClient<Database>(
-	process.env.NEXT_PUBLIC_SUPABASE_URL,
-	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-	{
-		auth: {
-			persistSession: true,
-			storageKey: 'sb-auth-token',
-			autoRefreshToken: false,
-			detectSessionInUrl: true,
-			flowType: 'pkce',
-			debug: false,
-			storage,
-		},
-	}
-);
+export const supabase = createClient<Database>(url, key, {
+	auth: {
+		persistSession: true,
+		storageKey: 'sb-auth-token',
+		autoRefreshToken: false,
+		detectSessionInUrl: true,
+		flowType: 'pkce',
+		debug: false,
+		storage,
+	},
+});
